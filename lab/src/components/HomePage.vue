d3d97f6340532aae709fe162042f85f46e1ce965
IEAD\2008017 <xuanjia961121@gmail.com>
2022-07-05 17:39:24 +0800

    fix lab

--- lab/src/components/HomePage.vue
+++ lab/src/components/HomePage.vue
@@ -1,6 +1,49 @@
 <template>
 <div>
-  Hello World
+
+  <!-- top -->
+  <div id="jumbotron" class="jumbotron jumbotron-fluid">
+    <div class="container">
+      <div class="row align-items-center">
+        <div class="col-4">
+          <font id="title">MMLab</font>
+        </div>
+      </div>
+    </div>
+  </div>
+  
+  <div class="container">
+    <div class="row mt-3">
+      <div class="col-4 mt-5">
+        <div class="card" style="width: 18rem;">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-4 mt-5">
+        <div class="card" style="width: 18rem;">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-4 mt-5">
+        <div class="card" style="width: 18rem;">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  
 </div>
 </template>
 
@@ -10,11 +53,11 @@
 export default {
   data() {
     return {
-
+      v
     };
   },
   methods: {
-
+  
   }, 
   computed:{
    
@@ -23,5 +66,15 @@ export default {
 </script>
 
 <style scoped>
-
+#jumbotron {
+  background-color: rgb(87, 92, 160);
+}
+#title {
+  color:aliceblue;
+  height: 60px;
+}
+#loginUser {
+  color: aliceblue;
+  font-size:20px;
+}
 </style>
