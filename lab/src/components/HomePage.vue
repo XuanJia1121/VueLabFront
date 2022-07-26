723a6c7d4aaccb38da0c3b90f48f8ba1f97962ec
IEAD\2008017 <xuanjia961121@gmail.com>
2022-07-26 17:35:58 +0800

    fix

--- lab/src/components/HomePage.vue
+++ lab/src/components/HomePage.vue
@@ -12,7 +12,7 @@
           <el-button v-if="!GET_USERINFO.token" @click="toLogin()" type="info" icon="el-icon-s-custom" round>登入</el-button>
           <el-button v-else type="warning" icon="el-icon-s-custom" @click="logoutBtn()" round>登出</el-button>
           <el-button type="primary" icon="el-icon-shopping-cart-1" circle></el-button>
-          <el-button type="primary" icon="el-icon-s-tools" circle></el-button>
+          <el-button @click="visible = !visible" id="showModal" type="primary" icon="el-icon-s-tools" circle></el-button>
         </div>
       </div>
     </div>
@@ -50,30 +50,35 @@
             </div>
           </div>
         </div>
-      </div>
-    </el-main>
-  </el-container>
-
-  <!-- modal -->
-    <div class="modal fade" id="userModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-      <div class="modal-dialog" role="document">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-              <span aria-hidden="true">&times;</span>
-            </button>
-          </div>
-          <div class="modal-body">
-            ...
-          </div>
-          <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-            <button type="button" class="btn btn-primary">Save changes</button>
+        <div class="row align-items-center">
+          <div class="col-6 d-flex justify-content-center">
+             <el-popover
+              placement="bottom"
+              width="600"
+              v-model="visible">
+              <p>編輯資料</p>
+              <div style="text-align: right; margin: 0">
+                <form>
+                  <div class="form-group row">
+                    <label for="staticEmail" class="col-sm-2 col-form-label">Email</label>
+                    <div class="col-sm-10">
+                      <input type="text" readonly class="form-control-plaintext" id="staticEmail" value="email@example.com">
+                    </div>
+                  </div>
+                  <div class="form-group row">
+                    <label for="inputPassword" class="col-sm-2 col-form-label">Password</label>
+                    <div class="col-sm-10">
+                      <input type="password" class="form-control" id="inputPassword" placeholder="Password">
+                    </div>
+                  </div>
+                </form>
+              </div>
+            </el-popover>
           </div>
         </div>
       </div>
-    </div>
+    </el-main>
+  </el-container>
 
 </div>
 </template>
@@ -86,7 +91,7 @@ import {mapGetters,mapActions} from 'vuex'
 export default {
   data() {
     return {
-
+      visible: false,
     }
   },
   methods: {
